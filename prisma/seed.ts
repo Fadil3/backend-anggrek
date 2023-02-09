92946ba05c2d5387e3cd9252a7bb13cea14152f0
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2023-02-09 12:06:00 +0700

    Create seed.ts

---
+++ prisma/seed.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from '@prisma/client'
+import { hashPassword } from '../src/modules/auth'
+const prisma = new PrismaClient()
+async function main() {
+  console.log('Start seeding ...')
+  const fadil = await prisma.user.create({
+    data: {
+      email: 'fadil@gmail.com',
+      name: 'fadil normal',
+      password: await hashPassword('fadil123'),
+    },
+  })
+  const fadilAdmin = await prisma.user.create({
+    data: {
+      email: 'fadilAdmin@gmail.com',
+      name: 'fadilAdmin',
+      password: await hashPassword('fadil123'),
+      role: 'ADMINISTRATOR',
+    },
+  })
+  const fadilSuperAdmin = await prisma.user.create({
+    data: {
+      email: 'fadilSuperAdmin@gmail.com',
+      name: 'fadilSuperAdmin',
+      password: await hashPassword('fadil123'),
+      role: 'SUPER_ADMINISTRATOR',
+    },
+  })
+
+  console.log('Seeding success...')
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
